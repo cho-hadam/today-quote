4d07228c293b42bec04a3be64a2eb5388cef85f8
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-10-12 19:16:54 +0900

    activity_main.xml rename

--- app/src/main/java/co/hadam/todayquote/QuoteMainActivity.kt
+++ app/src/main/java/co/hadam/todayquote/QuoteMainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class QuoteMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.quote_activity_main)
     }
 }
