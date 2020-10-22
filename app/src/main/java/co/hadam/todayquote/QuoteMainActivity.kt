ac769ec30ea09ab23d0e08a772924e866a6b2e52
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-10-22 20:36:54 +0900

    SharedPreferences

--- app/src/main/java/co/hadam/todayquote/QuoteMainActivity.kt
+++ app/src/main/java/co/hadam/todayquote/QuoteMainActivity.kt
@@ -1,5 +1,7 @@
 package co.hadam.todayquote
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +9,8 @@ import android.widget.TextView
 import kotlin.random.Random
 
 class QuoteMainActivity : AppCompatActivity() {
+    private lateinit var pref: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.quote_activity_main)
@@ -15,18 +19,34 @@ class QuoteMainActivity : AppCompatActivity() {
         val quoteText = findViewById<TextView>(R.id.quote_text)
         val quoteFrom = findViewById<TextView>(R.id.quote_from)
 
-        // 수정 가능한 Quote를 저장할 리스트 객체 생성 및 명언 객체 3개 넣어보기
-        var quotes = mutableListOf<Quote>()
-        quotes.add(Quote(1, "명언 내용 1", "출처 1"))
-        quotes.add(Quote(2, "명언 내용 2", "출처 2"))
-        quotes.add(Quote(3, "명언 내용 3", "출처 3"))
+        // 프리퍼런스 객체 가져오기 (파일 이름은 quotes)
+        pref = this.getSharedPreferences("quotes", Context.MODE_PRIVATE)
 
-        var randomIndex = Random.nextInt(0, 3)
-        var randomQuote = quotes[randomIndex]
-        // var randomQuote = quotes.get(randomIndex)
-
-        // 뷰 객체를 이용해서 출력
-        quoteText.text = randomQuote.text
-        quoteFrom.text = randomQuote.from
+//        // 수정 가능한 Quote를 저장할 리스트 객체 생성 및 명언 객체 3개 넣어보기
+//        var quotes = mutableListOf<Quote>()
+//        quotes.add(Quote(1, "명언 내용 1", "출처 1"))
+//        quotes.add(Quote(2, "명언 내용 2", "출처 2"))
+//        quotes.add(Quote(3, "명언 내용 3", "출처 3"))
+//
+//        var randomIndex = Random.nextInt(0, 3)
+//        var randomQuote = quotes[randomIndex]
+//        // var randomQuote = quotes.get(randomIndex)
+//
+//        // 뷰 객체를 이용해서 출력
+//        quoteText.text = randomQuote.text
+//        quoteFrom.text = randomQuote.from
+//
+//        val sp = this.getSharedPreferences("quotes", Context.MODE_PRIVATE)
+//        val editor = sp.edit()
+//        editor.putInt("key1", 1)
+//        editor.putString("key2", "Hello")
+//        editor.putBoolean("make_alarm", true)
+//        editor.putInt("volumn", 100)
+//
+//        editor.putString("key2", "World")
+//        editor.apply()
+//
+//        var value1 = sp.getInt("key1", -1)
+//        var value2 = sp.getString("key2", null)
     }
 }
